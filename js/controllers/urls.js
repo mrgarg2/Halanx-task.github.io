efff2887e7c67ce85c6db0586b88bfbe9e37c891
Sajal Garg <mr.garg2@gmail.com>
2018-07-13 07:53:29 +0530

    Numke and start again

--- js/controllers/urls.js
+++
@@ -1,9 +0,0 @@
-app.value('logInURL','https://api.halanx.com/rest-auth/login/');
-
-app.value('logOutURL','https://api.halanx.com/rest-auth/logout/');
-
-app.value('hoursURL','https://api.halanx.com/places/place/17/openinghours/');
-
-app.value('plotURL','https://api.halanx.com/stores/dashboard/plots/?visits=true&from_date=2018-01-01&to_date=2018-07-01');
-
-app.value('detailsURL','https://api.halanx.com/stores/');
